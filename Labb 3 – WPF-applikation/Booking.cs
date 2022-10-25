2be6439064f66ce96bd98fcb2d9935f05feecdd1
Jonathan Strand <jontepwn@gmail.com>
2022-10-25 14:26:49 +0200

    funkar inte men whatever

---
+++ Labb 3 – WPF-applikation/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3___WPF_applikation
+{
+    internal class Booking
+    {
+        public Booking(string name, string tableNumber, string date, string time)
+        {
+            this.Name = name;
+            this.TableNumber = tableNumber;
+            this.Date = date;
+            this.Time = time;
+        }
+        public string Name { get; set; }
+        public string TableNumber { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
--- Labb 3 – WPF-applikation/MainWindow.xaml.cs
+++ Labb 3 – WPF-applikation/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,50 @@ namespace Labb_3___WPF_applikation
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Booking> bookings = new List<Booking>()
+        {
+            new Booking("name", "tablenumber", "date", "time"),
+            new Booking("name", "tablenumber", "date", "time")
+        };
+        
+    
         public MainWindow()
         {
             InitializeComponent();
+            DisplayContent();
+        }
+        private void DisplayContent()
+        {
+            lbox_Bookings_Content.ItemsSource = null;
+
+            lbox_Bookings_Content.ItemsSource = bookings;
+        }
+        private void btn_Boka_Click(object sender, RoutedEventArgs e)
+        {
+            string input1 = txt_nameInput.Text;
+            string input2 = cmbox_Table.Text;
+            string input3 = btn_DatePicker.Text;
+            string input4 = cbox_Time.Text;
+           
+
+            bookings.Add(new Booking(input1, input2, input3, input4));
+;
+            
+            DisplayContent();
+        }
+
+        private void btn_Visa_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayContent();
+        }
+
+        private void btn_Avboka_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbox_Bookings_Content == null)
+                return;
+           // bookings.Remove(lbox_Bookings_Content.SelectedItem);
+            DisplayContent();
         }
     }
 }
