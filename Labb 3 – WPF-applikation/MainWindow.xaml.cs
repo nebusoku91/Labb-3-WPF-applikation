57dc9640443e426d977d910b5e71b02a52e268e3
Jonathan Strand <jontepwn@gmail.com>
2022-10-26 23:26:50 +0200

    holy moly

--- Labb 3 – WPF-applikation/MainWindow.xaml.cs
+++ Labb 3 – WPF-applikation/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
-using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Input;
 
 namespace Labb_3___WPF_applikation
 {
@@ -11,11 +11,11 @@ namespace Labb_3___WPF_applikation
     public partial class MainWindow : Window
     {
 
-        List<Booking> bookings = new List<Booking>() 
+        List<Booking> bookings = new List<Booking>()
         {
-        new Booking ("Jan Eriksson", "Bord 2", "2022-11-09", "13:00"),
-        new Booking ("Margareta Persson", "Bord 4", "2022-12-12", "16:00"),
-        new Booking ("Stina Lundgren", "Bord 5", "2022-10-22", "11:00"),
+        new Booking ("Jan Eriksson", "Bord 2", "2023-11-09", "13:00"),
+        new Booking ("Margareta Persson", "Bord 4", "2023-12-12", "16:00"),
+        new Booking ("Stina Lundgren", "Bord 5", "2023-10-22", "11:00"),
         };
 
         // Order =  Date > Name > Table > Time
@@ -24,6 +24,8 @@ namespace Labb_3___WPF_applikation
         {
             InitializeComponent();
             DisplayContent();
+            btn_DatePicker.DisplayDateStart = DateTime.Today;
+            Loaded += (s, e) => Keyboard.Focus(txt_nameInput);
         }
 
         private void DisplayContent()
@@ -39,36 +41,44 @@ namespace Labb_3___WPF_applikation
             }
             catch (Exception ex)
             {
-                System.Windows.MessageBox.Show("Error", ex.Message, MessageBoxButton.OK, MessageBoxImage.Information);
+                System.Windows.MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
             }
-            
+
         }
 
         private void btn_Boka_Click(object sender, RoutedEventArgs e)
         {
-            string input_Name = txt_nameInput.Text;
-            string input_Table = cmbox_Table.Text;
-            string input_Date = btn_DatePicker.Text;
-            string input_Time = cbox_Time.Text;
-            int x = 0;
-            if(input_Name == "" || input_Table == "" || input_Date == "" || input_Time == "")
+            try
             {
-                return;
-            }
-            foreach (Booking xy in bookings)
-            {
-                if (xy.Date == input_Date && xy.Time == input_Time && xy.TableNumber == input_Table)
-                {
 
-                    System.Windows.MessageBox.Show("I'm afraid I can't let you do that, David.", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                string input_Name = txt_nameInput.Text;
+                string input_Table = cbox_Table.Text;
+                string input_Date = btn_DatePicker.Text;
+                string input_Time = cbox_Time.Text;
+
+                if (input_Name == "" || input_Table == "" || input_Date == "" || input_Time == "")
+                {
+                    System.Windows.MessageBox.Show("Namn, Datum, Tid, och Bordsnummer behövs specificeras innan det kan läggas in i listan.", "Meddelande", MessageBoxButton.OK, MessageBoxImage.Information);
                     return;
                 }
 
+                foreach (Booking xy in bookings)
+                {
+                    if (xy.Date == input_Date && xy.Time == input_Time && xy.TableNumber == input_Table)
+                    {
+                        System.Windows.MessageBox.Show("Det angivna datumet, tiden, och bordet är redan bokade.", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+                }
 
+                bookings.Add(new Booking(input_Name, input_Table, input_Date, input_Time));
+                DisplayContent();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("I'm afraid I can't do that, David.\n" + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
             }
 
-            bookings.Add(new Booking(input_Name, input_Table, input_Date, input_Time));
-            DisplayContent();
         }
 
         private void btn_Visa_Click(object sender, RoutedEventArgs e)
@@ -78,13 +88,21 @@ namespace Labb_3___WPF_applikation
 
         private void btn_Avboka_Click(object sender, RoutedEventArgs e)
         {
-            int index = lbox_Bookings_Content.SelectedIndex;
-            if (lbox_Bookings_Content.SelectedItem == null)
-                return;
+            try
+            {
+                int index = lbox_Bookings_Content.SelectedIndex;
+                if (lbox_Bookings_Content.SelectedItem == null)
+                    return;
 
-            bookings.RemoveAt(index);
+                bookings.RemoveAt(index);
 
-            DisplayContent();
+                DisplayContent();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("I'm afraid I can't do that, David.\n" + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
     }
 }
