af8f469a4a67b270c23bf3e24b330cf716221b0a
Jonathan Strand <jontepwn@gmail.com>
2022-10-26 22:47:44 +0200

    hi ho silver

--- Labb 3 – WPF-applikation/MainWindow.xaml.cs
+++ Labb 3 – WPF-applikation/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Labb_3___WPF_applikation
 {
@@ -10,9 +11,14 @@ namespace Labb_3___WPF_applikation
     public partial class MainWindow : Window
     {
 
-        List<Booking> bookings = new List<Booking>();
+        List<Booking> bookings = new List<Booking>() 
+        {
+        new Booking ("Jan Eriksson", "Bord 2", "2022-11-09", "13:00"),
+        new Booking ("Margareta Persson", "Bord 4", "2022-12-12", "16:00"),
+        new Booking ("Stina Lundgren", "Bord 5", "2022-10-22", "11:00"),
+        };
 
-        // Order =  Name > Table > Date > Time
+        // Order =  Date > Name > Table > Time
 
         public MainWindow()
         {
@@ -22,12 +28,20 @@ namespace Labb_3___WPF_applikation
 
         private void DisplayContent()
         {
-            lbox_Bookings_Content.Items.Clear();
-
-            foreach (Booking x in bookings)
+            try
             {
-                lbox_Bookings_Content.Items.Add(String.Format("{0}, {1}, {2}, {3}.", x.Name, x.TableNumber, x.Date, x.Time));
+                lbox_Bookings_Content.Items.Clear();
+
+                foreach (Booking x in bookings)
+                {
+                    lbox_Bookings_Content.Items.Add(String.Format("{0}, {1}, {2}, kl. {3}.", x.Date, x.Name, x.TableNumber, x.Time));
+                }
             }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Error", ex.Message, MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            
         }
 
         private void btn_Boka_Click(object sender, RoutedEventArgs e)
@@ -36,14 +50,24 @@ namespace Labb_3___WPF_applikation
             string input_Table = cmbox_Table.Text;
             string input_Date = btn_DatePicker.Text;
             string input_Time = cbox_Time.Text;
+            int x = 0;
             if(input_Name == "" || input_Table == "" || input_Date == "" || input_Time == "")
             {
                 return;
             }
-            else
+            foreach (Booking xy in bookings)
             {
-                bookings.Add(new Booking(input_Name, input_Table, input_Date, input_Time));
+                if (xy.Date == input_Date && xy.Time == input_Time && xy.TableNumber == input_Table)
+                {
+
+                    System.Windows.MessageBox.Show("I'm afraid I can't let you do that, David.", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+
             }
+
+            bookings.Add(new Booking(input_Name, input_Table, input_Date, input_Time));
             DisplayContent();
         }
 
